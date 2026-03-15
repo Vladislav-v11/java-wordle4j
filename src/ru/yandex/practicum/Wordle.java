90608a5fa255467d14d95ab79d3c7ee925042b47
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-03-15 03:46:22 +0300

    Задание спринта 7.

--- src/ru/yandex/practicum/Wordle.java
+++ src/ru/yandex/practicum/Wordle.java
@@ -1,5 +1,12 @@
 package ru.yandex.practicum;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import ru.yandex.practicum.exceptions.*;
+
 /*
 в главном классе нам нужно:
     создать лог-файл (он должен передаваться во все классы)
@@ -11,8 +18,91 @@ package ru.yandex.practicum;
  */
 public class Wordle {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        PrintWriter log = null;
+
+        try {
+            log = createLogFile();
+            log.println("Запуск игры");
+            WordleDictionaryLoader loader = new WordleDictionaryLoader(log);
+            WordleDictionary dictionary = loader.loadDictionary("words_ru.txt");
+            WordleGame game = new WordleGame(dictionary, log);
+            playGame(game, log);
+        } catch (DictionaryUploadException e) {
+            System.err.println("Ошибка загрузки словаря " + e.getMessage());
+            if (log != null) {
+                log.println("Ошибка " + e.getMessage());
+                e.printStackTrace(log);
+            }
+        } catch (Exception e) {
+            System.err.println("Ошибка " + e.getMessage());
+            if (log != null) {
+                log.println("Ошибка " + e.getMessage());
+                e.printStackTrace(log);
+            }
+        } finally {
+            if (log != null) {
+                log.println("Завершение работы программы");
+                log.close();
+            }
+        }
     }
+
+    private static PrintWriter createLogFile() throws IOException {
+        String logFileName = "lod.txt";
+        FileWriter fileWriter = new FileWriter(logFileName, StandardCharsets.UTF_8);
+        return new PrintWriter(fileWriter, true);
+    }
+
+    private static void playGame(WordleGame game, PrintWriter log) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Отгадайте слово из 5 букв за 6 попыток");
+            System.out.println("Команды:");
+            System.out.println("  Enter - подсказка (осталось: " + game.getCountHints() + ")");
+            System.out.println("  'стоп' - выйти из игры");
+            log.println("Начало серии");
+
+            while (!game.isGameOver()) {
+                System.out.println("Введите слово");
+                String input = scanner.nextLine().trim();
+
+                if (game.isExitCommand(input)) {
+                    System.out.println("Игра остановлена");
+                    log.println("Игра принудительно завершена " + input);
+                    break;
+                }
+                try {
+                    if (input.isEmpty()) {
+                        String hint = game.getHint();
+                        System.out.println("Подсказка - " + hint);
+                        log.println("Запрошена подсказка " + hint);
+                        continue;
+                    }
+                    String result = game.makeGuess(input);
+
+                    System.out.println("Результат " + result);
+                    System.out.println("осталось попыток " + game.getSteps());
+
+                    if (game.isWon()) {
+                        System.out.println("Вы отгадали слово " + game.getAnswer());
+                        log.println("Игра выиграна. Загаданное слово - " + game.getAnswer());
+                        break;
+                    } else if (game.isGameOver()) {
+                        System.out.println("Игра проиграна. Загаданное слово " + game.getAnswer());
+                        log.println("Игра проиграна. Загаданное слово - " + game.getAnswer());
+                        break;
+                    }
+                } catch (ru.yandex.practicum.exceptions.InvalidWordException e) {
+                    System.out.println("Ошибка: " + e.getMessage());
+                    log.println("Введено некорректное слово: " + input);
+                } catch (ru.yandex.practicum.exceptions.WordNotFoundException e) {
+                    System.out.println("Ошибка: " + e.getMessage());
+                    log.println("Введено слово отсутствующее в словаре - " + input);
+                }
+            }
+        }
+    }
+
 
 }
--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,11 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.exceptions.WordNotFoundException;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -10,5 +15,68 @@ import java.util.List;
 public class WordleDictionary {
 
     private List<String> words;
+    private Random random = new Random();
+    private final PrintWriter log;
+
+    public WordleDictionary(List<String> words, PrintWriter log) {
+        this.words = new ArrayList<>(words);
+        this.log = log;
+    }
+
+    public String getRandomWord() {
+        //Random random = new Random();
+        String word = words.get(random.nextInt(words.size()));
+        log.println("Выбрано слово: " + word);
+        return word;
+    }
+
+    public static String normalizeWord(String word) {
+        return word.toLowerCase().replace('ё', 'е').trim();
+    }
+
+    public boolean containsWord(String word) throws WordNotFoundException {
+        if (word == null) {
+            return false;
+        }
+        log.println("Проверка наличия слова '" + word + "' в словаре: " + words.contains(normalizeWord(word)));
+        return words.contains(normalizeWord(word));
+    }
+
+    public static String compareWords(String guess, String answer) {
+        if (guess == null || guess.length() != 5) {
+            throw new IllegalArgumentException("Слово может состоять только из 5 букв ");
+        }
+        char[] result = new char[5];
+        boolean[] answerUsed = new boolean[5];
+        for (int i = 0; i < 5; i++) {
+            if (guess.charAt(i) == answer.charAt(i)) {
+                result[i] = '+';
+                answerUsed[i] = true;
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (result[i] == '+') {
+                continue;
+            }
+            char guessChar = guess.charAt(i);
+            boolean found = false;
+
+            for (int j = 0; j < 5; j++) {
+                if (!answerUsed[j] && answer.charAt(j) == guessChar) {
+                    result[i] = '^';
+                    answerUsed[j] = true;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[i] = '-';
+            }
+        }
+        return new String(result);
+    }
+    public List<String> getWords() {
+        return words;
+    }
 
 }
